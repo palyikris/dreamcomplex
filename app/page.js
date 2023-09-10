213487c23f6795c1c328c85ea950c2b4bedc6f01
Kristóf Pályi <palyi.kristof@gmail.com>
2023-09-10 23:50:01 +0200

    main page design ready mobile view wip

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
 import Topnav from "./../components/topnav/page";
+import { SocialIcon } from "react-social-icons";
 
 export default function Page() {
   let router = useRouter;
@@ -86,6 +87,128 @@ export default function Page() {
             voluptate tenetur iste quam assumenda quo, officia harum dolor,
             praesentium quibusdam sit possimus maiores.
           </p>
+          <div className={styles.sep}></div>
+          <button>Lehetőségek</button>
+        </div>
+      </div>
+      <div className={styles.bigTitle}>
+        <h1>Lehetőségeink:</h1>
+      </div>
+      <div className={styles.cards}>
+        <div className={styles.card}>
+          <div className={styles.cardPic}></div>
+          <div className={styles.description}>
+            <h1>Dream House</h1>
+            <div className={styles.cardDetails}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <p>
+                Kéne valami leírás, de lusta vagyok kitalálni bármit, és a lorem
+                ipsum meg túl hosszú
+              </p>
+            </div>
+            <button>Részletek</button>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardPic}></div>
+          <div className={styles.description}>
+            <h1>Dream Apartman</h1>
+            <div className={styles.cardDetails}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <p>
+                Kéne valami leírás, de lusta vagyok kitalálni bármit, és a lorem
+                ipsum meg túl hosszú
+              </p>
+            </div>
+            <button>Részletek</button>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardPic}></div>
+          <div className={styles.description}>
+            <h1>Dream Tópart</h1>
+            <div className={styles.cardDetails}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <p>
+                Kéne valami leírás, de lusta vagyok kitalálni bármit, és a lorem
+                ipsum meg túl hosszú
+              </p>
+            </div>
+            <button>Részletek</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.logoContainer}>
+          <div className={styles.logo}>Valami logo</div>
+          <div className={styles.logoDescription}>
+            <h1>Dream Konplexumok</h1>
+            <p>Nagyon örülünk, hogy minket választ a nyaralásához!</p>
+          </div>
+        </div>
+        <div className={styles.else}>
+          <div className={styles.documents}>
+            <p>Példa valami</p>
+            <p>Másik példa</p>
+          </div>
+          <div className={styles.social}>
+            <p>Kövess minket!</p>
+            <div className={styles.socialApps}>
+              <SocialIcon
+                url="https://twitter.com/"
+                fgColor="#daa06d"
+                bgColor="#FFFFFF"
+              ></SocialIcon>
+              <SocialIcon
+                url="https://facebook.com/"
+                fgColor="#daa06d"
+                bgColor="#FFFFFF"
+              ></SocialIcon>
+              <SocialIcon
+                url="https://instagram.com/"
+                fgColor="#daa06d"
+                bgColor="#FFFFFF"
+              ></SocialIcon>
+              <SocialIcon
+                url="https://tiktok.com/"
+                fgColor="#daa06d"
+                bgColor="#FFFFFF"
+              ></SocialIcon>
+            </div>
+          </div>
         </div>
       </div>
     </div>
