9435e87ea672051bfbe4284acdea7d05be3e79d2
palyikris <83282307+palyikris@users.noreply.github.com>
2023-12-06 22:49:05 +0100

    minor changes

--- components/reserveInterface/apartman/page.js
+++ components/reserveInterface/apartman/page.js
@@ -20,7 +20,6 @@ export default function ReserveApartmanComponent(props) {
             type="radio"
             onChange={() => {
               setApartmanNumber(1);
-              router.push("/reservation#data");
             }}
           />
           <label for="radio1" />
@@ -110,7 +109,6 @@ export default function ReserveApartmanComponent(props) {
             type="radio"
             onChange={() => {
               setApartmanNumber(2);
-              router.push("/reservation#data");
             }}
           />
           <label for="radio2" />
@@ -200,7 +198,6 @@ export default function ReserveApartmanComponent(props) {
             type="radio"
             onChange={() => {
               setApartmanNumber(3);
-              router.push("/reservation#data");
             }}
           />
           <label for="radio3" />
@@ -290,7 +287,6 @@ export default function ReserveApartmanComponent(props) {
             type="radio"
             onChange={() => {
               setApartmanNumber(4);
-              router.push("/reservation#data");
             }}
           />
           <label for="radio4" />
