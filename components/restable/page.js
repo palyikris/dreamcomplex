87f7c0db59843831b776b2ad21ef4a320f8f8a46
kribbany <palyi.kristof@gmail.com>
2024-06-13 23:03:20 +0200

    deleting reservations ready

--- components/restable/page.js
+++ components/restable/page.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import {
+  DeleteReservation,
   FetchApartmanDataByApartmanType,
   SetDocumentSeen
 } from "@/lib/firebase";
@@ -12,12 +13,22 @@ export default function ReservationTable(props) {
   let [isLoading, setIsLoading] = useState(true);
   let [nOfRows, setNOfRows] = useState(10);
 
+  const HandleDeletion = (type, id) => {
+    let confirm = window.confirm("Biztosan törölni szeretnéd?");
+    if (confirm) {
+      DeleteReservation(type, id).then(() => {
+        setNOfRows(nOfRows - 1);
+      });
+    }
+  };
+
   useEffect(
     () => {
       FetchApartmanDataByApartmanType(type).then(data => {
         let sortedData = data.sort((a, b) => {
           return new Date(b.arrDate) - new Date(a.arrDate);
         });
+        setNOfRows(sortedData.length);
         sortedData = sortedData.slice(0, nOfRows);
         setReservations(sortedData);
         setIsLoading(false);
@@ -52,6 +63,7 @@ export default function ReservationTable(props) {
               onChange={e => {
                 setNOfRows(e.target.value);
               }}
+              placeholder={reservations.length}
             />
           </div>
         </div>
@@ -65,6 +77,7 @@ export default function ReservationTable(props) {
     <div className={styles.reservationTable}>
       <div className={styles.reservation}>
         <div className={styles.reservationData}>
+          <div />
           <div className={styles.data}>Név</div>
           <div className={styles.data}>Email</div>
           <div className={styles.data}>Telefon</div>
@@ -82,6 +95,7 @@ export default function ReservationTable(props) {
             onChange={e => {
               setNOfRows(e.target.value);
             }}
+            placeholder={reservations.length}
           />
         </div>
       </div>
@@ -89,6 +103,27 @@ export default function ReservationTable(props) {
         return (
           <div className={styles.reservation} key={i}>
             <div className={styles.reservationData}>
+              <div className={styles.deleteWrapper}>
+                <button
+                  className={styles.delete}
+                  onClick={() => {
+                    HandleDeletion(type, reservation.id);
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
               <div className={styles.data}>
                 <p>
                   {reservation.name}
